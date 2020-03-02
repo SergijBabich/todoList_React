6dcb041a36d488d888fe157b9f327cc7a3ab7413
Segrij Babich <moran.a@list.ru>
2020-03-02 20:46:17 +0200

    Updates

---
+++ precache-manifest.1cd93e522327db49188a611dc42e9373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e07464448bb0117479cccb539c1cc70",
+    "url": "/todoList_React/index.html"
+  },
+  {
+    "revision": "72a96187c8df4a76c042",
+    "url": "/todoList_React/static/css/main.3cda0c12.chunk.css"
+  },
+  {
+    "revision": "18956fdf99c5d4393e0a",
+    "url": "/todoList_React/static/js/2.93e6a1d8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todoList_React/static/js/2.93e6a1d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72a96187c8df4a76c042",
+    "url": "/todoList_React/static/js/main.4d160601.chunk.js"
+  },
+  {
+    "revision": "a98fd9c3b777073d3e21",
+    "url": "/todoList_React/static/js/runtime-main.1548be10.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoList_React/static/media/logo.5d5d9eef.svg"
+  }
+]);
